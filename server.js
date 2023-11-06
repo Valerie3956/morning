5dac3efa0d56a00788955ca8d00a17a3bf7b42ce
Valerie Smith <valerie.plante.11@gmail.com>
2023-11-06 12:25:29 -0500

    added build folder

--- server.js
+++ server.js
@@ -5,10 +5,11 @@ const mongoose = require('mongoose')
 require('dotenv').config()
 const URL = process.env.MONGO_URL
 console.log(URL)
-
+const path = require("path")
 
 app.use(express.json())
 app.use(morgan('dev'))
+app.use(express.static(path.join(__dirname, "client", "build")))
 
 
 app.use("/morning/journal", require("./routes/journalRouter.js"))
@@ -19,6 +20,11 @@ mongoose.connect(URL, () => console.log('connected to database'))
 app.use((err, req, res, next) => {
     console.log(err)
     return res.send({errMsg: err.message})
+})
+
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 })
 
 app.listen(9000, () => console.log("The server is running on port 9000"))
