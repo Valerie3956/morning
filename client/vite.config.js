3a8195b4f124263728519b2992c28dc18919e6bc
Valerie Smith <valerie.plante.11@gmail.com>
2023-11-06 12:51:43 -0500

    changed proxy

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
+const isDevEnv = false
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/morning': {
-        target: 'http://127.0.0.1:9000',
+        target: !isDevEnv? "https://valerie3956morning.app" : 'http://127.0.0.1:9000',
         changeOrigin: true,
         secure: false,
       },
